adfa5ca9703674fd3a56212a6d4f6971c8df07e9
Jose Annunziato <jannunzi@gmail.com>
2024-09-05 20:53:46 -0400

    getting started on a1

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Welcome to Web App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
