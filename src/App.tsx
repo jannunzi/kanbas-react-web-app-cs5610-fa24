a5ebda56ef6321cd6c70472f63cc14a2215473c0
Jose Annunziato <jannunzi@gmail.com>
2024-09-12 21:59:25 -0400

    make kanbas the default screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Kanbas from "./Kanbas";
 // import "./App.css";
 import Labs from "./Labs";
-import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Link, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
+          <Route path="/" element={<Navigate to="/Kanbas" />} />
           <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
