e359f43b8ceeb0c8a8525958f0eed78ff96bb178
Jose Annunziato <jannunzi@gmail.com>
2024-09-14 19:23:52 -0400

    make lab1 default

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import TOC from "./TOC";
@@ -10,6 +10,7 @@ export default function Labs() {
       <h1>Labs</h1>
       <TOC />
       <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<h2>Lab 3</h2>} />
