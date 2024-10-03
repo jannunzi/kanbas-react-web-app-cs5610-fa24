f0c7b15930066f6c5866ec5f551c9a862407dc5a
Jose Annunziato <jannunzi@gmail.com>
2024-10-03 19:00:38 -0400

    a5

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,16 @@
+export default function Lab5() {
+  return (
+    <div id="wd-lab5">
+      <h2>Lab 5</h2>
+      <div className="list-group">
+        <a
+          href="http://localhost:4000/lab5/welcome"
+          className="list-group-item"
+        >
+          Welcome
+        </a>
+      </div>
+      <hr />
+    </div>
+  );
+}
